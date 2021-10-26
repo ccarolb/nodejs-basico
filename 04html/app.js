025c7c331a278ba1bd33bf684571a57aa094e99f
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-10-25 21:42:29 -0300

    trabalhando com arquivos estáticos

--- 04html/app.js
+++ 04html/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//Especificando arquivos estáticos
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html') //sendFile não precisa de .end
 })
